9febc80e7bc3508df7e66b021a86785843669b2c
Lukas Kesch <lukas.Kesch@gmail.com>
2020-04-21 14:53:08 +0200

    Add project files.

---
+++ PathFindingAlgorithms/AStar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PathFindingAlgorithms
+{
+    public partial class MainWindow : Window
+    {
+        private void MenuItemAStarAlgorithm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ PathFindingAlgorithms/File.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PathFindingAlgorithms
+{
+    public partial class MainWindow : Window
+    {
+        private void MenuItemFileCreate_Click(object sender, RoutedEventArgs e)
+        {
+            GroupBoxSettings.Visibility = Visibility.Visible;
+        }
+
+        private void MenuItemFileLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItemFileSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
